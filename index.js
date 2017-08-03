a0ad98b954a62c9994eae4c2ccfcfbeb2dd1b2fb
Sammy Steiner <sammysteiner@gmail.com>
2017-08-03 12:49:45 -0400

    built out several functions, mocked out others

--- index.js
+++ index.js
@@ -7,7 +7,22 @@ function navigateDucks(){
   // get pond boundaries
   var boundaries = document.getElementById('pond_size').value.split(' ')
   // split the duck lists into an array of duck info arrays
-  var duck_instructions = document.getElementById('duck_info').value.split(/\r?\n|\r/)
+  var duckPuts = document.getElementById('duck_info').value.split(/\r?\n|\r/)
+  var inDuckStions = []
+  for (var i = 0; i < duckPuts.length; i++) {
+    if (i === 0 || i % 2 === 0) {
+      inDuckStions.push(duckPuts[i].concat(duckPuts[i + 1]))
+    }
+  }
+  var whereAreTheyNow = []
+  if (validateInputs(boundaries, duckPuts)) {
+    for(var i = 0; i < inDuckStions.length; i++){
+      var movement = inDuckStions[i].slice(2, inDuckStions[i].length - 1)
+      if (validateMovement(coords, movement, boundaries)) {
+        moveDuck([inDuckStions[i][0], inDuckStions[i][1], inDuckStions[i][2]], movement, boundaries)
+      }
+    }
+  }
   // loop over the array and for each duck with a map function
     // modify their starting position based on the inputs
     // validate that the duck doesn't go out of bounds
@@ -18,12 +33,53 @@ function navigateDucks(){
   viewOutput()
 }
 
-function moveDuck(coords, movement){
-
+function moveDuck(coords, movement, boundaries){
+  var newCoords = coords
+  var dirS = {
+    'N': 'E',
+    'E': 'S',
+    'S': 'W',
+    'W': 'N'
+  }
+  var dirP = {
+    'N': 'W',
+    'E': 'N',
+    'S': 'E',
+    'W': 'S'
+  }
+  movement.forEach( m => {
+    if (m === 'P') {
+      newCoords[2] = dirP[coords[2]]
+    } else if (m === 'S') {
+      newCoords[2] = dirS[coords[2]]
+    } else {
+      if (validateMovement(coords, m, boundaries)) {
+        switch (coords[2]) {
+          case 'N':
+            newCoords[1] = coords[1] + 1
+            break
+          case 'S':
+            newCoords[1] = coords[1] - 1
+            break
+          case 'E':
+            newCoords[0] = coords[0] + 1
+            break
+          case 'W':
+            newCoords[0] = coords[0] - 1
+            break
+        }
+      }
+    }
+  })
+  return newCoords
 }
 
-function validateMovement(coords, movement, boundaries){
-  
+function validateInputs(boundaries, duckPuts){
+  return true
+}
+
+function validateMovement(coords, m, boundaries){
+  return true
 }
 
 function viewOutput(){
